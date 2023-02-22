8fb65c17229412dee57b9fd8f47f2ff7958b879c
Бобырев Дмитрий <bobirev.dm@esphere.ru>
2023-02-23 03:54:56 +0500

    Add login modal story

---
+++ src/features/AuthByUsername/ui/LoginModal.stories.tsx
@@ -0,0 +1,32 @@
+import { type ComponentMeta, type ComponentStory } from '@storybook/react'
+import { LoginModal } from './LoginModal'
+import { THEME } from 'shared/config/theme'
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator'
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator'
+import { action } from '@storybook/addon-actions'
+
+const meta: ComponentMeta<typeof LoginModal> = {
+    title: 'features/LoginModal',
+    component: LoginModal,
+}
+export default meta
+
+const Template: ComponentStory<typeof LoginModal> = (args) => {
+    return <LoginModal {...args} />
+}
+
+export const Primary = Template.bind({})
+Primary.args = {
+    isOpen: true,
+    onClose: action('onClose'),
+    onLoginSuccess: action('onLoginSuccess'),
+}
+Primary.decorators = [StoreDecorator({})]
+
+export const Dark = Template.bind({})
+Dark.args = {
+    isOpen: true,
+    onClose: action('onClose'),
+    onLoginSuccess: action('onLoginSuccess'),
+}
+Dark.decorators = [StoreDecorator({}), ThemeDecorator(THEME.DARK)]
